b5942c1bbcd58a66e6137c3efb11555b6675eb2e
JahidulSany <jahidulsanypro@gmail.com>
2024-05-05 19:19:16 +0100

    Form Struture and Design Implemented

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 const getChildId = (children) => {
@@ -11,10 +12,10 @@ const getChildId = (children) => {
 const Field = ({ label, children, htmlFor, error }) => {
   const id = htmlFor || getChildId(children);
   return (
-    <div>
-      {label && <label htmlFor={id}>{label}</label>}
+    <div className='flex flex-col items-start justify-start mt-2 p-0 w-full mr-2'>
+      {label && <label htmlFor={id} className='mb-1'>{label}</label>}
       {children}
-      {error && <div>{error.message}</div>}
+      {error && <div className='text-red-500'>{error.message}</div>}
     </div>
   );
 };
--- src/forms/LoginForm.jsx
+++ src/forms/LoginForm.jsx
@@ -1,6 +1,35 @@
+import Field from '../components/Field';
+import FieldSet from '../components/FieldSet';
+
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
-  )
-}
-export default LoginForm
+    <div>
+      <form>
+        <FieldSet label="Enter Login Details">
+          <Field label="Email">
+            <input
+              className="p-2 border box-border w-[300px] border-gray-200 rounded-md"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter email address"
+            />
+          </Field>
+          <Field label="Password">
+            <input
+              className="p-2 border box-border w-[300px] border-gray-200 rounded-md"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter Passsword"
+            />
+          </Field>
+          <Field>
+            <button className='p-2 border text-md text-white cursor-pointer rounded-lg bg-purple-500 w-[100px]' type="submit">Login</button>
+          </Field>
+        </FieldSet>
+      </form>
+    </div>
+  );
+};
+export default LoginForm;
